b98279379282193ff267f67fcc100e8512846e17
WZEfilter <wze.business1@gmail.com>
2025-07-30 04:21:10 +0800

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,22 @@
+import SerpApi from 'serpapi';
+const serp = new SerpApi.GoogleSearch(process.env.SERPAPI_KEY);
+
+export async function searchProduct(partName) {
+  const json = await serp.json({
+    engine:  'amazon',
+    q:       partName,
+    country: 'US'
+  });
+  const top = json.shopping_results?.[0] || {};
+  if (!top.link) {
+    return { part: partName, error: 'No result found' };
+  }
+  const url = new URL(top.link);
+  url.searchParams.set('tag', process.env.AFFILIATE_TAG);
+  return {
+    part:  partName,
+    title: top.title,
+    price: top.price,
+    link:  url.toString()
+  };
+}
